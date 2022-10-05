70ccb730243eab2ce4fbe79c16f2bce880d77c0f
shindonghwi <ehdgnl8940@naver.com>
2022-10-05 20:05:37 +0900

    fix: 중복 변수명 변경

--- application/api/__init__.py
+++ application/api/__init__.py
@@ -9,22 +9,23 @@ def allowed_file(filename):
 
 
 def check_file(request_file, save_folder):
+    response = None
     try:
         if 'file' not in request_file:
-            resp = jsonify({'message': 'No file part in the request'})
-            resp.status_code = 400
+            response = jsonify({'message': 'No file part in the request'})
+            response.status_code = 400
         file = request_file['file']
         if file.filename == '':
-            resp = jsonify({'message': 'No file selected for uploading'})
-            resp.status_code = 400
+            response = jsonify({'message': 'No file selected for uploading'})
+            response.status_code = 400
         if file and allowed_file(file.filename):
             file.save("{}/{}".format(save_folder, secure_filename(file.filename)))
-            resp = jsonify({'message': 'File successfully uploaded'})
-            resp.status_code = 200
+            response = jsonify({'message': 'File successfully uploaded'})
+            response.status_code = 200
         else:
-            resp = jsonify({'message': 'Allowed file types are aac, mp4, wav'})
-            resp.status_code = 400
+            response = jsonify({'message': 'Allowed file types are aac, mp4, wav'})
+            response.status_code = 400
     except:
         pass
     finally:
-        return resp
+        return response
