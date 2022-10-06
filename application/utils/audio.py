a6d6b86d34de591a94d41b32a6686b8acd8d9110
shindonghwi <ehdgnl8940@naver.com>
2022-10-06 15:08:37 +0900

    fix: Librosa 매개변수 변경

--- application/utils/audio.py
+++ application/utils/audio.py
@@ -6,7 +6,7 @@ import soundfile
 def load_librosa(audio_path, sample_rate=16000) -> dict:
     librosa_dict = {"y": None, "sr": None}
     try:
-        y, sr = librosa.load(audio_path, sr=sample_rate, duration=5, offset=30)
+        y, sr = librosa.load(audio_path, sr=sample_rate)
         librosa_dict["y"] = y
         librosa_dict["sr"] = sr
     except:
