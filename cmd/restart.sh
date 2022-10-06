f292e176957de74428f7ac4d1da3db94ee35a39f
shindonghwi <ehdgnl8940@naver.com>
2022-10-06 19:07:31 +0900

    feat: flask restart.sh add

---
+++ cmd/restart.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd /home/ec2-user/workspace/mago-demo-api-healthcare/cmd
+python3 run_check.py
+
---
+++ cmd/run_check.py
@@ -0,0 +1,15 @@
+import os
+
+process_read = os.popen("ps -ef | grep run.py | grep -v 'grep'").readlines()
+# ps -ef 명령어를 이용해서 현재 프로세스를 출력한 후, 그 중 run24h.py 문자열이 포함된 줄만 모은다.
+# grep 명령어 자체도 프로세스에 나타나므로 grep -v를 이용해서 제외한다.
+check_process = str(process_read)
+text_location = check_process.find("run.py")
+# run24h.py가 몇번째 문자열인지 찾아낸다. 만약 문자열이 없으면, 즉 프로세스가 존재하지 않을 경우에는 -1을 반환한다.
+if (text_location == -1):
+    print("Process not found!")
+    os.system("python3 /home/ec2-user/workspace/mago-demo-api-healthcare/run.py &")
+    # 해당 프로그램을 다시 실행한다. 백그라운드에서 실행할 경우 &기호를 붙인다.
+    print("Program restarted!")
+else:
+    print("Process exists. Location is", text_location)
