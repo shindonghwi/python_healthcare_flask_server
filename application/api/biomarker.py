fdb68866bf26031f947676bc510e74605cdbfbe0
shindonghwi <ehdgnl8940@naver.com>
2022-10-07 11:27:11 +0900

    fix: feats 파일 용량제한

--- application/api/biomarker.py
+++ application/api/biomarker.py
@@ -47,10 +47,11 @@ def extrack_audio_file():
         audio_file_info = get_file_info(request.files['audio'])
         feats_file_info = get_file_info(request.files['feats'])
 
-        if feats_file_info["length"] > 1000000:
-            response["status"] = 510
-            response["msg"] = "(Not Extend) - Too Large Json File .."
-            return response
+        print(feats_file_info["length"])
+        # if feats_file_info["length"] > 1000000:
+        #     response["status"] = 510
+        #     response["msg"] = "(Not Extend) - Too Large Json File .."
+        #     return response
 
         # audio 파일에서 그래프 추출
         librosa_dict = load_librosa(audio_path="{}/{}".format(save_folder, audio_file_info["full_name"]))
